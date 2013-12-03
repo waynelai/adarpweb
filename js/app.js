0ccaa5c378c4f8d0b38fb28dd23bfa37ed0b1eb3
waynelai <wenchenglai@gmail.com>
2013-12-03 15:16:37 -0500

    added more fake content

--- js/app.js
+++ js/app.js
@@ -1,15 +1,12 @@
-window.Seewamee = window.Ember.Application.create();
+window.App = window.Ember.Application.create();
 
-window.Seewamee.Router.map(function() {
-    this.resource('index', { path: '/' }, function () {
-        this.route('contact');
-        this.route('about');
-    });
+window.App.Router.map(function() {
+    this.route('index', { path: '/' });
     this.route('contact', { path: '/contact' });
     this.route('about', { path: '/about' });
 });
 
-window.Seewamee.IndexRoute = window.Ember.Route.extend({
+window.App.IndexRoute = window.Ember.Route.extend({
   model: function() {
     return ['red', 'yellow', 'blue'];
   }
