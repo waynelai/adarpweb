9cccb13c381adc32e92d03bc034abf044071dfa7
waynelai <wenchenglai@gmail.com>
2014-01-23 03:31:10 -0500

    v2

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ window.App.Router.map(function() {
     this.route('index', { path: '/' });
     this.route('contact', { path: '/contact' });
     this.route('about', { path: '/about' });
+    this.route('xiwami');
+    this.route('analysis');
+    this.route('acquisition');
+    this.route('visualization');
 });
 
 window.App.IndexRoute = window.Ember.Route.extend({
